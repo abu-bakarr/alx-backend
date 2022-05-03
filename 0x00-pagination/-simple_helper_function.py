8878c69d10c2784b3c03ea2c7d4e89b712067df5
abu-bakarr <abangura14@yahoo.com>
2022-05-03 08:48:23 +0000

    Add pagination

--- 0x00-pagination/-simple_helper_function.py
+++ 0x00-pagination/-simple_helper_function.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 """
-Simple Helper Function
+Helper
 """
 
+from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
-    """index"""
-    return (page - 1) * page_size, page * page_size
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """[return the range of pagination]
+    Args:
+        page (int): [number page]
+        page_size (int): [size of each page]
+    Returns:
+        Tuple[int, int]: [tuple of pagination parameters]
+    """
+    return ((page_size*page) - page_size, page_size*page)
